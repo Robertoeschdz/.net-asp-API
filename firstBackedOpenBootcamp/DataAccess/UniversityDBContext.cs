874f9b8d79dd1450659c2daa122f1bea02480ceb
roberto <robertoeschdz@gmail.com>
2023-06-22 00:36:06 -0600

    Add project files.

---
+++ firstBackedOpenBootcamp/DataAccess/UniversityDBContext.cs
@@ -0,0 +1,19 @@
+﻿using firstBackedOpenBootcamp.Models.DataModels;
+using Microsoft.EntityFrameworkCore;
+
+namespace firstBackedOpenBootcamp.DataAccess
+{
+    public class UniversityDBContext: DbContext
+    {
+        public UniversityDBContext(DbContextOptions<UniversityDBContext> options): base(options)
+        { 
+
+        }
+
+        public DbSet<User>? Users { get; set; }
+        public DbSet<Course>? Courses { get; set; }
+        public DbSet<Category>? Categories { get; set; }
+        public DbSet<Student>? Students { get; set; }
+        public DbSet<Chapter>? Chapter { get; set; }
+    }
+}
---
+++ firstBackedOpenBootcamp/Models/DataModels/BaseEntity.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace firstBackedOpenBootcamp.Models.DataModels
+{
+    public class BaseEntity
+    {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+        public string CreatedBy { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public string UpdatedBy { get; set; } = string.Empty;
+        public DateTime? UpdatedAt { get; set; }
+        public string DeletdBy { get; set; } = string.Empty;
+        public DateTime? DeletedAt { get; set; }
+        public bool IsDeleted { get; set; } = false;
+    }
+}
